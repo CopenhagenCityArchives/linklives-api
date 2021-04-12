85366cc51d917abfca96810a2ca803aa394ee096
Lars Høgedal <lars.hogedal@gmail.com>
2021-04-12 16:39:59 +0200

    fixed solution folder structure

---
+++ linklives-api-dal/domain/Link.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linklives_api_dal.domain
+{
+    public class Link
+    {
+        public int link_id { get; set; }
+        public string link_key { get; set; }
+        public int iteration { get; set; }
+        public int iteration_inner { get; set; }
+        public int method_id { get; set; }
+        public int pa_id1 { get; set; }
+        public double score { get; set; }
+        public int pa_id2 { get; set; }
+        public int source_id1 { get; set; }
+        public int source_id2 { get; set; }
+        public string method_type { get; set; }
+        public string method_subtype1 { get; set; }
+        public string method_description { get; set; }
+    }
+}
