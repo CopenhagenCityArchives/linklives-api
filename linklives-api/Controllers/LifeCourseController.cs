e0c611d9d5766b533c0fd876f4278930c6ae6f0a
Lars Høgedal <lars.hogedal@gmail.com>
2021-04-14 14:35:58 +0200

    preliminary crud controller implementation

---
+++ linklives-api/Controllers/LifeCourseController.cs
@@ -0,0 +1,78 @@
+﻿using linklives_api_dal.domain;
+using linklives_api_dal.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace linklives_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LifeCourseController : ControllerBase
+    {
+        private readonly ILifeCourseRepository repository;
+
+        public LifeCourseController(ILifeCourseRepository repository)
+        {
+            this.repository = repository;
+        }
+        // GET: LifeCourse/5
+        [HttpGet]
+        public ActionResult Get(string id)
+        {
+            var result = repository.GetLifeCourseByID(id);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+        }
+
+        // PUT: LifeCourse/
+        [HttpPut]
+        [Authorize]
+        public ActionResult Put([FromBody]LifeCourse lifeCourse)
+        {
+            try
+            {
+                repository.InsertLifeCourse(lifeCourse);
+                repository.Save();
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        // PUT: LifeCourse/BulkInsert
+        [HttpPut("BulkInsert")]
+        [Authorize]
+        public ActionResult BulkInsert([FromBody]IEnumerable<LifeCourse> lifeCourses)
+        {
+            try
+            {
+                repository.InsertLifeCourses(lifeCourses);
+                repository.Save();
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        // DELETE: LifeCourse/5
+        [HttpDelete]
+        [Authorize]
+        public ActionResult Delete(string id)
+        {
+            repository.DeleteLifeCourse(id);
+            repository.Save();
+            return Ok();
+        }
+    }
+}
