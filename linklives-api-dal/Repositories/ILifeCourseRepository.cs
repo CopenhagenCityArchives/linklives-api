34ffc88d230fda8b9ed5ec7227019846e489bec0
Lars Høgedal <lars.hogedal@gmail.com>
2021-04-14 13:36:21 +0200

    scafolded repositories

---
+++ linklives-api-dal/Repositories/ILifeCourseRepository.cs
@@ -0,0 +1,18 @@
+﻿using linklives_api_dal.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linklives_api_dal.Repositories
+{
+    public interface ILifeCourseRepository
+    {
+        IEnumerable<LifeCourse> GetLifeCourses();
+        LifeCourse GetLifeCourseByID(int lifeCourseKey);
+        void InsertLifeCourses(IEnumerable<LifeCourse> lifeCourses);
+        void DeleteLifeCourse(int lifeCourseKey);
+        void Save();
+    }
+}
---
+++ linklives-api-dal/Repositories/ILinkRepository.cs
@@ -0,0 +1,19 @@
+﻿using linklives_api_dal.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linklives_api_dal.Repositories
+{
+    public interface ILinkRepository
+    {
+        IEnumerable<Link> GetLinks();
+        Link GetLinkByID(int linkKey);
+        void InsertLink(Link link);
+        void InsertLinks(IEnumerable<Link> links);
+        void DeleteLink(int linkKey);
+        void Save();
+    }
+}
