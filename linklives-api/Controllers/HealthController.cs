4a66a76a8e8517913ac0e571e12a9138560c66c7
Lars Høgedal <lars.hogedal@gmail.com>
2021-04-14 13:11:17 +0200

    added health controller for isAlive pings.

---
+++ linklives-api/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace linklives_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("I am alive!");
+        }
+    }
+}
