1ee7d44ac635c6a92eac0fd388ceddf8ebcdc516
Lars Høgedal <lars.hogedal@gmail.com>
2021-04-26 15:14:58 +0200

    preliminary elastic search implementation

--- linklives-api-dal/Repositories/ESPersonAppearanceRepository.cs
+++ linklives-api-dal/Repositories/ESPersonAppearanceRepository.cs
@@ -1,22 +1,49 @@
-﻿using linklives_api_dal.domain;
+﻿using Elasticsearch.Net;
+using linklives_api_dal.domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace linklives_api_dal.Repositories
 {
     public class ESPersonAppearanceRepository : IPersonAppearanceRepository
     {
+        private readonly ElasticLowLevelClient client;
+
+        public ESPersonAppearanceRepository(ElasticLowLevelClient client)
+        {
+            this.client = client;
+        }
+
         public PersonAppearance GetById(int PaId)
         {
-            throw new NotImplementedException();
+            return JsonSerializer.Deserialize<PersonAppearance>(GetRawJsonById(PaId));
         }
 
         public string GetRawJsonById(int PaId)
         {
             throw new NotImplementedException();
+            //TODO: Write proper query
+            var searchResponse = client.Search<StringResponse>("people", PostData.Serializable(new
+            {
+                from = 0,
+                size = 10,
+                query = new
+                {
+                    match = new
+                    {
+                        firstName = new
+                        {
+                            query = "Martijn"
+                        }
+                    }
+                }
+            }));
+
+            return searchResponse.Body;
         }
     }
 }
