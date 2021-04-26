eac62216b0e241fe9149fc79431840f3af0ba029
Lars Høgedal <lars.hogedal@gmail.com>
2021-04-26 14:07:57 +0200

    Added personappaerance controller and setup enriching links with pernappearances with failsafe

---
+++ linklives-api-dal/domain/PersonAppearance.cs
@@ -0,0 +1,12 @@
+﻿namespace linklives_api_dal.domain
+{
+    public class PersonAppearance
+    {
+        public string name_display { get; set; }
+        public string birthyear_display { get; set; }
+        public string role_display { get; set; }
+        public string birthplace_display { get; set; }
+        public string occupation_display { get; set; }
+        public string sourceplace_display { get; set; }
+    }
+}
---
+++ linklives-api/Controllers/PersonAppearanceController.cs
@@ -0,0 +1,38 @@
+﻿using linklives_api_dal.domain;
+using linklives_api_dal.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace linklives_api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonAppearanceController : ControllerBase
+    {
+        private readonly IPersonAppearanceRepository repository;
+
+        public PersonAppearanceController(IPersonAppearanceRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet("{id}")]
+        [HttpGet("{key}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public ActionResult Get(int id)
+        {
+            var result = repository.GetRawJsonById(id);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+        }
+    }
+}
