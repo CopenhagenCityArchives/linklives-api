19e43ef92dd2d2861eac774d3bb3a8476fcaba29
Lars Høgedal <lars.hogedal@gmail.com>
2021-04-26 15:14:58 +0200

    preliminary elastic search implementation

--- linklives-api-dal/domain/PersonAppearance.cs
+++ linklives-api-dal/domain/PersonAppearance.cs
@@ -1,12 +1,20 @@
-﻿namespace linklives_api_dal.domain
+﻿using Newtonsoft.Json;
+
+namespace linklives_api_dal.domain
 {
     public class PersonAppearance
     {
-        public string name_display { get; set; }
-        public string birthyear_display { get; set; }
-        public string role_display { get; set; }
-        public string birthplace_display { get; set; }
-        public string occupation_display { get; set; }
-        public string sourceplace_display { get; set; }
+        [JsonProperty(PropertyName = "name_display")]
+        public string Name_display { get; set; }
+        [JsonProperty(PropertyName = "birthyear_display")]
+        public string Birthyear_display { get; set; }
+        [JsonProperty(PropertyName = "role_display")]
+        public string Role_display { get; set; }
+        [JsonProperty(PropertyName = "birthplace_display")]
+        public string Birthplace_display { get; set; }
+        [JsonProperty(PropertyName = "occupation_display")]
+        public string Occupation_display { get; set; }
+        [JsonProperty(PropertyName = "sourceplace_display")]
+        public string Sourceplace_display { get; set; }
     }
 }
