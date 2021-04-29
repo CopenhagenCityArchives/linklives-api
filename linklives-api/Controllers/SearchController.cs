98211698f03fb58a662a0dee43d5f479d7bd734c
Lars Høgedal <lars.hogedal@gmail.com>
2021-04-29 11:32:21 +0200

    implemented search endpoint

--- linklives-api/Controllers/SearchController.cs
+++ linklives-api/Controllers/SearchController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Elasticsearch.Net;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace linklives_api.Controllers
@@ -11,5 +13,18 @@ namespace linklives_api.Controllers
     [ApiController]
     public class SearchController : ControllerBase
     {
+        private readonly ElasticLowLevelClient client;
+
+        public SearchController(ElasticLowLevelClient client)
+        {
+            this.client = client;
+        }
+        [HttpPost("{indexes}")]
+        public IActionResult Get(string indexes,[FromBody]JsonElement query)
+        {
+            var searchResponse = client.Search<StringResponse>(indexes, query.ToString());
+
+            return StatusCode(searchResponse.HttpStatusCode.HasValue ? searchResponse.HttpStatusCode.Value : 0, searchResponse.Body);
+        }
     }
 }
