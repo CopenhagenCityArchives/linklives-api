a70b0665d8ad4ba8ee63fdee8f86a068bb3bb5d6
Lars Høgedal <lars.hogedal@gmail.com>
2021-06-14 10:36:45 +0200

    add rating choice repositroy

---
+++ linklives-api-dal/Repositories/EFRatingChoiceRepository.cs
@@ -0,0 +1,23 @@
+﻿using linklives_api_dal.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linklives_api_dal.Repositories
+{
+    public class EFRatingChoiceRepository : IRatingChoiceRepository
+    {
+        private readonly LinklivesContext context;
+        public IEnumerable<RatingChoice> GetAll()
+        {
+            return context.RatingChoices;
+        }
+
+        public RatingChoice GetById(int id)
+        {
+            return context.RatingChoices.Single(x => x.Id == id);
+        }
+    }
+}
