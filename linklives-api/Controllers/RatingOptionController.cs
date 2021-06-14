1cf45450c6e3d5b48af98d3d43fffec960e2fe7f
Lars Høgedal <lars.hogedal@gmail.com>
2021-06-14 13:49:23 +0200

    added ratingoption controller

---
+++ linklives-api/Controllers/RatingOptionController.cs
@@ -0,0 +1,42 @@
+﻿using linklives_api_dal.domain;
+using linklives_api_dal.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace linklives_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RatingOptionController : Controller
+    {
+        private readonly IRatingOptionRepository repository;
+
+        public RatingOptionController(IRatingOptionRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet()]
+        [ProducesResponseType(typeof(List<RatingOption>), 200)]
+        public IActionResult Get()
+        {
+            return Ok(repository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(RatingOption), 200)]
+        [ProducesResponseType(404)]
+        public ActionResult Get(int id)
+        {
+            var result = repository.GetById(id);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+        }
+    }
+}
