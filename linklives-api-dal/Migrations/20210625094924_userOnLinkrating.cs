e617b8e0b4953879c1007dcd475f419d715b8997
Lars Høgedal <lars.hogedal@gmail.com>
2021-06-25 11:54:44 +0200

    new db migration for userid on linkrating

---
+++ linklives-api-dal/Migrations/20210625094924_userOnLinkrating.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace linklives_api_dal.Migrations
+{
+    public partial class userOnLinkrating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "User",
+                table: "LinkRatings",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "User",
+                table: "LinkRatings");
+        }
+    }
+}
