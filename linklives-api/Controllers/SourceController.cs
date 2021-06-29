08205296a4964936cfa4e3f8f659b47b5b4a343a
Lars Høgedal <lars.hogedal@gmail.com>
2021-06-29 09:48:18 +0200

    Fixed datatypes on source

---
+++ linklives-api/Controllers/SourceController.cs
@@ -0,0 +1,28 @@
+﻿using linklives_api_dal.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace linklives_api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SourceController : ControllerBase
+    {
+        private readonly ISourceRepository repository;
+
+        public SourceController(ISourceRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpPost("~/sources")]
+        public IActionResult GetAll()
+        {
+            return Ok(repository.GetAll());
+        }
+    }
+}
