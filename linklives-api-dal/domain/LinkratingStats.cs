5e961afcf69a4cfd043673589f421224cb4ef8fc
Lars Høgedal <lars.hogedal@gmail.com>
2021-08-03 15:26:25 +0200

    added linkrating stats endpoint

---
+++ linklives-api-dal/domain/LinkratingStats.cs
@@ -0,0 +1,19 @@
+﻿using linklives_api_dal.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linklives_api_dal.domain
+{
+    public class LinkRatingStats
+    {
+        public LinkRatingStats()
+        {
+            HeadingRatings = new Dictionary<string, int>();
+        }
+        public Dictionary<string, int> HeadingRatings { get; set; }
+        public int TotalRatings { get; set; }
+    }
+}
