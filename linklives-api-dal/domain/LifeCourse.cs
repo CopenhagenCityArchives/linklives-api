79013077ed2b533e87e9e31216ce2402d0161e75
Lars Høgedal <lars.hogedal@gmail.com>
2021-08-05 14:00:49 +0200

    fixed some problems wiht mapping of ES data

--- linklives-api-dal/domain/LifeCourse.cs
+++ linklives-api-dal/domain/LifeCourse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace linklives_api_dal.domain
     {
         public int Life_course_id { get; set; }
         public virtual ICollection<Link> Links { get; set; }
+        [NotMapped]
         public List<PersonAppearance> PersonAppearances {get; set;}
     }
 }
