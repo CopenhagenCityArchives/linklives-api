20245cf6cb8d6ab6531f7c0c7aa70043c3b2fffe
Lars Høgedal <lars.hogedal@gmail.com>
2021-08-05 09:52:50 +0200

    removed api from the url of stats controller

--- linklives-api/Controllers/StatsController.cs
+++ linklives-api/Controllers/StatsController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace linklives_api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class StatsController : ControllerBase
     {
