8bc322df830450437ae2eb7bba5b6bf4bd40caf9
Bo Henriksen <stumpdenmark@hotmail.com>
2022-05-06 15:01:46 +0200

    Update test project references

--- linklives-api-Test/LifecourseTests.cs
+++ linklives-api-Test/LifecourseTests.cs
@@ -8,7 +8,7 @@ namespace linklives_api_Test
     [TestFixture]
     public class LifecourseTests
     {
-        private ILifeCourseRepository lc_repo;
+        private IEFLifeCourseRepository lc_repo;
         private IPersonAppearanceRepository pa_repo;
         private ISourceRepository source_repo;
         private LifeCourseController controller;
@@ -16,10 +16,10 @@ namespace linklives_api_Test
         [SetUp]
         public void Setup()
         {
-            var lc_repo = new Mock<ILifeCourseRepository>();
+            var lc_repo = new Mock<IEFLifeCourseRepository>();
             var pa_repo = new Mock<IPersonAppearanceRepository>();
             var source_repo = new Mock<ISourceRepository>();
-            var controller = new LifeCourseController(lc_repo.Object, pa_repo.Object, source_repo.Object);
+         //   var controller = new LifeCourseController(lc_repo.Object, pa_repo.Object, source_repo.Object);
         }
 
         [Test]
