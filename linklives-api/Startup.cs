a3c0fab2c46581104a5a76461add9b22d0a34f72
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-11-25 14:47:24 +0100

    Allow Content-Disposition header to be read from browsers

--- linklives-api/Startup.cs
+++ linklives-api/Startup.cs
@@ -153,7 +153,7 @@ namespace linklives_api
             //app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors();
+            app.UseCors(builder => builder.WithExposedHeaders("Content-Disposition"));
 
             app.UseAuthentication();
             app.UseAuthorization();
